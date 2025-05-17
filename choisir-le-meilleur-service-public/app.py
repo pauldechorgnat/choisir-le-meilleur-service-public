8e4af431cd35b59faf20dd0c4a3ac810ba878e72
pauldechorgnat <23296889+pauldechorgnat@users.noreply.github.com>
2025-05-17 16:24:47 +0200

    OPS: ajout de docker compose

--- choisir-le-meilleur-service-public/app.py
+++ choisir-le-meilleur-service-public/app.py
@@ -1,14 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, abort
 
 from mock import get_offering, search_offering, parse_input
 
 app = Flask(__name__)
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
 @app.get("/")
 def render_search():
     if "page-number" in request.args:
-        page_number = request.args["page-number"]
+        page_number: int = int(request.args["page-number"])
     else:
         page_number = 0
     if "search-query" in request.args:
@@ -52,7 +57,7 @@ def render_search():
 def get_offering_page(offering_id: str):
     offering = get_offering(offering_id=offering_id)
     if offering is None:
-        raise ValueError("Erreur 404") # TODO: faire mieux
+        abort(404)
     return render_template(
         "offering.html",
         offering=offering,
@@ -60,4 +65,35 @@ def get_offering_page(offering_id: str):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=2022, debug=True)
+    from argparse import ArgumentParser
+
+    argument_parser = ArgumentParser(
+        description="Application pour Choisir le meilleur service public",
+    )
+    argument_parser.add_argument(
+        "--host",
+        help="Application host",
+        default="0.0.0.0",
+    )
+
+    argument_parser.add_argument(
+        "-p",
+        "--port",
+        help="Application port",
+        default=2022,
+        type=int,
+    )
+    argument_parser.add_argument(
+        "-d",
+        "--debug",
+        action="store_true",
+        help="Debug mode",
+    )
+
+    arguments = argument_parser.parse_args()
+
+    app.run(
+        host=arguments.host,
+        port=arguments.port,
+        debug=arguments.debug,
+    )
